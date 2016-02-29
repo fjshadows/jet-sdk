e51ebd94ac1d19fd2fa8654f2288c7e2d4bf9ef4
Nikita Z <nikita_z@progforce.com>
2016-02-29 12:37:10 +0300

    add getting sku list

--- test/products.js
+++ test/products.js
@@ -4,7 +4,9 @@ var Jet = require('../');
 var params = {
   SKU: 'testsku',
   user: 'test',
-  pass: 'test'
+  pass: 'test',
+  offset: 'testsku',
+  limit: 1
 }
 
 function GetProductBySKU(params) {
@@ -16,4 +18,13 @@ function GetProductBySKU(params) {
   return client.auth().then(client=>client.invoke(req));
 }
 
+function GetProductSKU(params) {
+  var client = new Jet.Client(params.user, params.pass, {host:'localhost:3021'});
+  var req = Jet.Products.requests.GetProductSKU();
+  req.set('offset', params.offset);
+  req.set('limit', params.limit);
+  return client.auth().then(client=>client.invoke(req));
+}
+
+GetProductSKU(params);
 GetProductBySKU(params);
